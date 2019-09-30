fc6e5908442f8f590a55878df1697d5f905b12a5
carbarah <44706505+carbarah@users.noreply.github.com>
2019-09-30 01:03:12 -0600

    1
    CTE524

---
+++ geoweb/0813Campos.js
@@ -0,0 +1,15 @@
+var campos ={
+"type": "FeatureCollection",
+"name": "0813Campos",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 28, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -87.37283493636879, 13.994450927177176 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 122, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -87.353288078923967, 13.970021877325692 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 319, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -87.340901821612647, 13.935283569262634 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 398, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -87.321663060462669, 13.93134596999119 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 405, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -87.412704965277825, 13.931017764710662 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 423, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -87.434826858310757, 13.93041756686458 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 476, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -87.280903494615174, 13.890092543179366 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 528, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -87.301336578398676, 13.863151280400636 ] } }
+]
+}
---
+++ geoweb/0813CentroSaludOjojona.js
@@ -0,0 +1,10 @@
+var salud ={
+"type": "FeatureCollection",
+"name": "0813CentroSaludOjojona",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::32616" } },
+"features": [
+{ "type": "Feature", "properties": { "Cod_US": 914.0, "Tip_Nom_US": "(CMO) OJOJONA", "Nom_US": "OJOJONA", "Tipo_US": "CMO", "Cod_Reg": 8.0, "Nom_Reg": "Departamental de Francisco Morazß", "Cod_Area": null, "Nom_Area": "Centro", "Cod_Dep": "08", "Nom_Dep": "Francisco Morazßn", "Cod_Mun": "081300", "Nom_Mun": "Ojona", "Cd_Ald_Loc": null, "Nom_Ald_Lo": "Ojojona", "Fecha_crea": "01\/01\/2000", "Y": 1540219.28003, "X": 467893.42901000002, "Fecha_modi": "26\/05\/2010", "INSAME2015": "NO", "Cambio": null }, "geometry": { "type": "Point", "coordinates": [ 467893.42901, 1540219.28003 ] } },
+{ "type": "Feature", "properties": { "Cod_US": 7641.0, "Tip_Nom_US": "(CSR) GUASUCARAN", "Nom_US": "GUASUCARAN", "Tipo_US": "CSR", "Cod_Reg": 8.0, "Nom_Reg": "Departamental de Francisco Morazß", "Cod_Area": null, "Nom_Area": "Centro", "Cod_Dep": "08", "Nom_Dep": "Francisco Morazßn", "Cod_Mun": "081300", "Nom_Mun": "Ojona", "Cd_Ald_Loc": null, "Nom_Ald_Lo": "Guasucarßn", "Fecha_crea": "01\/01\/2000", "Y": 1533165.09415, "X": 460230.09837999998, "Fecha_modi": "26\/05\/2010", "INSAME2015": "NO", "Cambio": null }, "geometry": { "type": "Point", "coordinates": [ 460230.0983799999813, 1533165.09415 ] } },
+{ "type": "Feature", "properties": { "Cod_US": 80420.0, "Tip_Nom_US": "(CSR) EL AGUACATAL", "Nom_US": "EL AGUACATAL", "Tipo_US": "CSR", "Cod_Reg": 8.0, "Nom_Reg": "Departamental de Francisco Morazß", "Cod_Area": null, "Nom_Area": "Centro", "Cod_Dep": "08", "Nom_Dep": "Francisco Morazßn", "Cod_Mun": "081300", "Nom_Mun": "Ojona", "Cd_Ald_Loc": null, "Nom_Ald_Lo": "El Aguacatal", "Fecha_crea": "16\/08\/2001", "Y": 1547142.2726, "X": 460251.71567000001, "Fecha_modi": "26\/05\/2010", "INSAME2015": "NO", "Cambio": null }, "geometry": { "type": "Point", "coordinates": [ 460251.71567, 1547142.2726 ] } }
+]
+}
